83250f87b7574b006edfdfa91066cfb2f2ce794e
Hitt <hitler.natividad@unas.edu.pe>
2024-12-23 21:37:50 -0500

    V 0.0.27 personal

--- backend/almacen/middleware.py
+++ backend/almacen/middleware.py
@@ -3,9 +3,14 @@ class CORSMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        response = self.get_response(request)
+        if request.method == "OPTIONS":
+            response = self.get_response(request)
+        else:
+            response = self.get_response(request)
+            
         response["Access-Control-Allow-Origin"] = "http://localhost:3000"
         response["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With"
+        response["Access-Control-Max-Age"] = "86400"  # 24 horas
         response["Access-Control-Allow-Credentials"] = "true"
         return response 
