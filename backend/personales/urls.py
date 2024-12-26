52abca36f2c744aa92fe96d3b40c46bd1ae0517e
Hitt <hitler.natividad@unas.edu.pe>
2024-12-25 20:52:11 -0500

    modify user roles

--- backend/personales/urls.py
+++ backend/personales/urls.py
@@ -3,11 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .views import PersonalViewSet, TareoViewSet, PersonalSearchView, PersonalListView
 
 router = DefaultRouter()
-router.register(r'personal', PersonalViewSet)
-router.register(r'tareos', TareoViewSet)
+router.register(r'personal', PersonalViewSet, basename='personal')
+router.register(r'tareos', TareoViewSet, basename='tareo')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('personal/search/', PersonalSearchView.as_view(), name='personal-search'),
-    path('personal-list/', PersonalListView.as_view(), name='personal-list'),
+    path('buscar-personal/', PersonalSearchView.as_view(), name='buscar-personal'),
+    path('lista-personal/', PersonalListView.as_view(), name='lista-personal'),
 ] 
