40c5d01b7f29a6ea518b93c52938c90d228a2d02
Hitt <hitler.natividad@unas.edu.pe>
2024-12-28 01:42:40 -0500

    0.1.31 modify tareos v4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { TareoProvider } from './context/TareoContext';
 import AppRoutes from './routes/AppRoutes';
 import Navbar from './components/common/Navbar';
 
 function App() {
     return (
-        <TareoProvider>
-            <Router>
-                <div className="min-h-screen bg-gray-100">
-                    <Navbar />
-                    <main className="container mx-auto px-4 py-8">
-                        <Toaster position="top-right" />
-                        <AppRoutes />
-                    </main>
-                </div>
-            </Router>
-        </TareoProvider>
+        <Router>
+            <div className="min-h-screen bg-gray-100">
+                <Navbar />
+                <main className="container mx-auto px-4 py-8">
+                    <Toaster position="top-right" />
+                    <AppRoutes />
+                </main>
+            </div>
+        </Router>
     );
 }
 
